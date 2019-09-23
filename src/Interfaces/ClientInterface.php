26acf7ec2650be45c18ce873ebdaa615a3c618d7
Mark Radford <=>
2019-09-23 14:49:09 +0100

    Update the interface

--- src/Interfaces/ClientInterface.php
+++ src/Interfaces/ClientInterface.php
@@ -2,7 +2,9 @@
 
 namespace Commadore\GraphQLClient\Interfaces;
 
+use Commadore\GraphQLClient\GraphQLResponse;
+
 interface ClientInterface
 {
-
+    function query(string $query, array $variables = [], array $headers = []): GraphQLResponse;
 }
